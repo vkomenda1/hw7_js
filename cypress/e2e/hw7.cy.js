e914905487df595ed6ddc2c1ced9e1c6c3099130
vkomenda1 <wkomenda@gmail.com>
2023-10-30 12:28:01 +0200

    Removed the click() command and left the type() command

--- cypress/e2e/hw7.cy.js
+++ cypress/e2e/hw7.cy.js
@@ -8,21 +8,21 @@ describe('registration-tests', () => {
   it('registration-tests', () => {
     cy.get('#accountFrm_accountregister').first().check();
     cy.get('button[title="Continue"]').click();
-    cy.get('#AccountFrm_firstname').click().type('First_Name_test');
-    cy.get('#AccountFrm_lastname').click().type('Last_Name_test');
-    cy.get('#AccountFrm_email').click().type('mepere5511@ibtrades.com');
-    cy.get('#AccountFrm_telephone').click().type('0981115522');
-    cy.get('#AccountFrm_fax').click().type('380981115522');
-    cy.get('#AccountFrm_company').click().type('Qalight');
-    cy.get('#AccountFrm_address_1').click().type('Khreschatyk St.1');
-    cy.get('#AccountFrm_address_2').click().type('Khreschatyk St.2');
-    cy.get('#AccountFrm_city').click().type('Kyiv');
-    cy.get('#AccountFrm_postcode').click().type('00001');
+    cy.get('#AccountFrm_firstname').type('First_Name_test');
+    cy.get('#AccountFrm_lastname').type('Last_Name_test');
+    cy.get('#AccountFrm_email').type('mepere5511@ibtrades.com');
+    cy.get('#AccountFrm_telephone').type('0981115522');
+    cy.get('#AccountFrm_fax').type('380981115522');
+    cy.get('#AccountFrm_company').type('Qalight');
+    cy.get('#AccountFrm_address_1').type('Khreschatyk St.1');
+    cy.get('#AccountFrm_address_2').type('Khreschatyk St.2');
+    cy.get('#AccountFrm_city').type('Kyiv');
+    cy.get('#AccountFrm_postcode').type('00001');
     cy.get('#AccountFrm_country_id').select('220');
     cy.get('#AccountFrm_zone_id').select('3490');
-    cy.get('#AccountFrm_loginname').click().type('WK_test');
-    cy.get('#AccountFrm_password').click().type('123456');
-    cy.get('#AccountFrm_confirm').click().type('123456');
+    cy.get('#AccountFrm_loginname').type('WK_test');
+    cy.get('#AccountFrm_password').type('123456');
+    cy.get('#AccountFrm_confirm').type('123456');
     cy.get('#AccountFrm_newsletter1').check();
     cy.get('#AccountFrm_agree').check();
     cy.get('button[title="Continue"]').click();
@@ -31,8 +31,8 @@ describe('registration-tests', () => {
 
 describe('authorization-tests', () => {
   it('authorization-tests', () => {
-    cy.get('#loginFrm_loginname').click().type('WK_test');
-    cy.get('#loginFrm_password').click().type('123456');
+    cy.get('#loginFrm_loginname').type('WK_test');
+    cy.get('#loginFrm_password').type('123456');
     cy.get('button[title="Login"]').click();
   });
 });
